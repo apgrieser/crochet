4816478ca837624ea7dceb3ae8942d444b1e5c7f
Andrea Grieser <apgrieser@gmail.com>
2024-04-26 18:11:18 -0400

    Changed the layout of the hero section

--- script.js
+++ script.js
@@ -3,29 +3,52 @@
 //   window.scrollTo(0, 0);
 // };
 
-window.onscroll = function() {scrollFunction()};
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log("DOMContentLoaded");
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("return-to-top").style.display = "block";
-    } else {
-        document.getElementById("return-to-top").style.display = "none";
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("return-to-top").style.display = "block";
+        } else {
+            document.getElementById("return-to-top").style.display = "none";
+        }
     }
-}
 
-document.getElementById("return-to-top").addEventListener("click", function() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.getElementById("return-to-top").addEventListener("click", function() {
+        // console.log("return-to-top pressed")
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
+
+
+    // document.getElementById("get-started").addEventListener("click", function() {
+    //     console.log("get-started pressed")
+    //     document.getElementById("services").scrollIntoView({ behavior: 'smooth' });
+    // });
+
+    
+    // React when the "Get Started" button is pressed.
+    // var getStarted = document.querySelector("#get-started");
+    // console.log("After getStarted Set", getStarted)
+    // var servicesSection = document.querySelector("#services");
+    // console.log("After servicesSection Set", servicesSection);
+
+    // getStarted.addEventListener("click", function() {
+    //     console.log("Before scrollIntoView call ")
+    //     servicesSection.scrollIntoView({ behavior: "smooth" });
+    // });
+
+    // React when the "Learn More" button is pressed.
+    // document.addEventListener("DOMContentLoaded", function() {
+    //     var getStarted = document.querySelector("#learn-more-button");
+    //     var servicesSection = document.querySelector("#services");
+  
+    //     getStarted.addEventListener("click", function() {
+    //         servicesSection.scrollIntoView({ behavior: "smooth" });
+    //     });
+    //   });
+
+
 });
-
-// React when the "Learn More" button is pressed.
-// document.addEventListener("DOMContentLoaded", function() {
-//   var learnMoreButton = document.querySelector("#learn-more-button");
-//   var servicesSection = document.querySelector("#services");
-
-//   learnMoreButton.addEventListener("click", function() {
-//       servicesSection.scrollIntoView({ behavior: "smooth" });
-//   });
-// });
-
-
